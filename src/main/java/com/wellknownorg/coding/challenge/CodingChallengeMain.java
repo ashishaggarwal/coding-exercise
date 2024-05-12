a5156fc59781166a498e06c66cd3b765b55b317c
ashishaggarwal <ashishaggarwal1983@gmail.com>
2024-05-12 19:37:32 +0100

    Added main class for running the application as spring boot application

---
+++ src/main/java/com/wellknownorg/coding/challenge/CodingChallengeMain.java
@@ -0,0 +1,12 @@
+package com.wellknownorg.coding.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengeMain {
+
+    public static void main (String [] args) {
+        SpringApplication.run(CodingChallengeMain.class, args);
+    }
+}

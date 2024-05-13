cdcb9954c2fffdef224e9ec534edbfa458b9a7e2
ashishaggarwal <ashishaggarwal1983@gmail.com>
2024-05-13 13:46:45 +0100

    Added processor to calculate count of males, oldest person and age difference for two persons

--- src/main/java/com/wellknownorg/coding/challenge/CodingChallengeMain.java
+++ src/main/java/com/wellknownorg/coding/challenge/CodingChallengeMain.java
@@ -1,17 +1,28 @@
 package com.wellknownorg.coding.challenge;
 
 import com.wellknownorg.coding.challenge.file.read.FileReader;
+import com.wellknownorg.coding.challenge.processor.PersonProcessor;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public record CodingChallengeMain(FileReader fileReader) {
+public class CodingChallengeMain {
+
+    @Autowired
+    private FileReader fileReader;
+
+    @Autowired
+    private PersonProcessor personProcessor;
 
     @PostConstruct
     public void run() {
         fileReader.readFile("input.csv");
+        System.out.println(personProcessor.countMales());
+        System.out.println(personProcessor.findOldestPerson());
+        System.out.println(personProcessor.calculateAgeDiff("Bill McKnight", "Paul Robinson"));
     }
 
     public static void main (String [] args) {
